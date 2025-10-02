533bfd9918bfcf14bd3e40e5a515b5b47f96dd3c
Jakob Haahr Taankvist <jht@uber.com>
2025-10-02 13:31:09 +0200

    Rename
    Signed-off-by: Jakob Haahr Taankvist <jht@uber.com>

--- service/sharddistributor/store/etcd/module.go
+++ service/sharddistributor/store/etcd/module.go
@@ -21,7 +21,7 @@ type StoreParams struct {
 	ShardToExecutorCache *shardcache.ShardToExecutorCache
 }
 
-func NewCombined(p StoreParams) store.Store {
+func NewStore(p StoreParams) store.Store {
 	return &Store{
 		ExecutorStore:        p.Store,
 		ShardToExecutorCache: p.ShardToExecutorCache,
@@ -32,7 +32,7 @@ var Module = fx.Module("etcd",
 	fx.Provide(executorstore.NewStore),
 	fx.Provide(shardcache.NewShardToExecutorCache),
 	fx.Provide(leaderstore.NewLeaderStore),
-	fx.Provide(NewCombined),
+	fx.Provide(NewStore),
 	fx.Invoke(func(store executorstore.ExecutorStore, cache *shardcache.ShardToExecutorCache, lc fx.Lifecycle) {
 		lc.Append(fx.StartStopHook(cache.Start, cache.Stop))
 		lc.Append(fx.StartStopHook(store.Start, store.Stop))
