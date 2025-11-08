bc0a2710ea6a9edae7e3ada8a1f67f97beaf1f3d
Jakob Haahr Taankvist <jht@uber.com>
2025-11-08 16:27:53 +0100

    First implementation of the server side
    Signed-off-by: Jakob Haahr Taankvist <jht@uber.com>

--- service/sharddistributor/handler/handler.go
+++ service/sharddistributor/handler/handler.go
@@ -142,5 +142,51 @@ func (h *handlerImpl) assignEphemeralShard(ctx context.Context, namespace string
 }
 
 func (h *handlerImpl) WatchNamespaceState(request *types.WatchNamespaceStateRequest, server WatchNamespaceStateServer) error {
-	return fmt.Errorf("not implemented")
+	h.startWG.Wait()
+
+	// Subscribe to state changes from storage
+	// TODO: We need a better interface, this subscribes to too many things
+	versionChan, err := h.storage.Subscribe(server.Context(), request.Namespace)
+	if err != nil {
+		return fmt.Errorf("subscribe to namespace state: %w", err)
+	}
+
+	for range versionChan {
+		// Get the current state after receiving a version update
+		state, err := h.storage.GetState(server.Context(), request.Namespace)
+		if err != nil {
+			return fmt.Errorf("get state: %w", err)
+		}
+
+		// Convert storage state to response format
+		response := toWatchNamespaceStateResponse(state)
+
+		err = server.Send(response)
+		if err != nil {
+			return fmt.Errorf("send response: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func toWatchNamespaceStateResponse(state *store.NamespaceState) *types.WatchNamespaceStateResponse {
+	response := &types.WatchNamespaceStateResponse{
+		Executors: make([]*types.ExecutorShardAssignment, 0, len(state.ShardAssignments)),
+	}
+
+	for executorID, assignment := range state.ShardAssignments {
+		// Extract shard IDs from the assigned shards map
+		shardIDs := make([]string, 0, len(assignment.AssignedShards))
+		for shardID := range assignment.AssignedShards {
+			shardIDs = append(shardIDs, shardID)
+		}
+
+		response.Executors = append(response.Executors, &types.ExecutorShardAssignment{
+			ExecutorID:     executorID,
+			AssignedShards: shardIDs,
+			Metadata:       state.Executors[executorID].Metadata,
+		})
+	}
+	return response
 }
