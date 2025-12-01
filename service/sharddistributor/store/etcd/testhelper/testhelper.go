442aded0bc604888e3f079624bca8e720cd5845d
Jakob Haahr Taankvist <jht@uber.com>
2025-12-01 15:36:06 +0100

    linting
    Signed-off-by: Jakob Haahr Taankvist <jht@uber.com>

--- service/sharddistributor/store/etcd/testhelper/testhelper.go
+++ service/sharddistributor/store/etcd/testhelper/testhelper.go
@@ -16,7 +16,7 @@ import (
 	"github.com/uber/cadence/common/config"
 	shardDistributorCfg "github.com/uber/cadence/service/sharddistributor/config"
 	"github.com/uber/cadence/service/sharddistributor/store/etcd/etcdkeys"
-	//"github.com/uber/cadence/testflags"
+	"github.com/uber/cadence/testflags"
 )
 
 type StoreTestCluster struct {
@@ -30,7 +30,7 @@ type StoreTestCluster struct {
 func SetupStoreTestCluster(t *testing.T) *StoreTestCluster {
 	t.Helper()
 	flag.Parse()
-	//testflags.RequireEtcd(t)
+	testflags.RequireEtcd(t)
 
 	namespace := fmt.Sprintf("ns-%s", strings.ToLower(t.Name()))
 
