73cb52c527067033e1c759b49b32781a3324e7a1
Jakob Haahr Taankvist <jht@uber.com>
2026-04-17 10:40:22 +0200

    feat(sharddistributor): add metrics to canary pinger and shard creator
    Add tally-based metrics to the shard distributor canary service to
    enable alerting on canary health. Previously all signals were log-only.
    
    Metrics added:
    - canary_ping_success/failure/ownership_mismatch counters
    - canary_ping_latency histogram
    - canary_shard_created counter
    
    Signed-off-by: Jakob Haahr Taankvist <jht@uber.com>

---
+++ service/sharddistributor/canary/metrics/metrics.go
@@ -0,0 +1,36 @@
+package metrics
+
+import (
+	"time"
+
+	"github.com/uber-go/tally"
+)
+
+const (
+	// Counter metrics
+	CanaryPingSuccess            = "canary_ping_success"
+	CanaryPingFailure            = "canary_ping_failure"
+	CanaryPingOwnershipMismatch  = "canary_ping_ownership_mismatch"
+	CanaryShardCreated           = "canary_shard_created"
+	CanaryShardCreateFailure     = "canary_shard_create_failure"
+
+	// Histogram metrics
+	CanaryPingLatency = "canary_ping_latency"
+)
+
+var (
+	CanaryPingLatencyBuckets = tally.DurationBuckets([]time.Duration{
+		1 * time.Millisecond,
+		5 * time.Millisecond,
+		10 * time.Millisecond,
+		25 * time.Millisecond,
+		50 * time.Millisecond,
+		100 * time.Millisecond,
+		200 * time.Millisecond,
+		500 * time.Millisecond,
+		1 * time.Second,
+		2 * time.Second,
+		5 * time.Second,
+		10 * time.Second,
+	})
+)
